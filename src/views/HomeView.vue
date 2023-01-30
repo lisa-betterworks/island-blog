1a9bc21127e20de854363c3d5a408ea1559c76cd
Lisa Copeland <lisa.copeland@betterworks.com>
2023-01-30 12:44:03 -0500

    template refs in setup function

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,29 @@
 <template>
   <div class="home">
     <h2>Home</h2>
-    <p>Name: {{ name }} and Age: {{ age }}</p>
+    <p ref="p">Name: {{ name }} and Age: {{ age }}</p>
     <button @click="handleClick">click me</button>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
+
 export default {
   name: "HomeView",
   setup() {
+    const p = ref(null);
+
     let name = "Lisa";
     let age = 30;
 
     const handleClick = () => {
-      console.log("you clicked me");
+      console.log(p, p.value);
+      p.value.classList.add("msg");
+      p.value.textContent = "hello islanders";
     };
 
-    return { name, age, handleClick };
+    return { name, age, handleClick, p };
   },
   created() {
     console.log("created");
@@ -28,4 +34,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.msg {
+  color: aqua;
+}
+</style>
